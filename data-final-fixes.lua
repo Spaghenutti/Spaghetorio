d6db923003bb9dcb0efc2b32681a67ea9bd37162
Spaghenutti <147646299+Spaghenutti@users.noreply.github.com>
2023-12-21 16:43:55 +0100

    added final fixes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,4 @@
 -- require("compatibility.krastorio-final-fixes")
 -- require("compatibility.vanilla-furnace")
 -- require("compatibility.aai-industry-fixes")
+require("compatibility.aai-and-krastorio-final-fixes")
--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,3 @@
 -- require("prototypes.entity-changes")
 -- require("compatibility.vanilla-furnace")
 require("compatibility.aai-industry-fixes")
-require("compatibility.aai-and-krastorio-fixes")
