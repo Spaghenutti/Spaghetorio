dd8a852b0074bfea4a44267107e325fdcd3850bf
Spaghenutti <147646299+Spaghenutti@users.noreply.github.com>
2023-12-23 16:36:49 +0100

    fixed recepie locations

--- compatibility/aai-and-krastorio-recipe.lua
+++ compatibility/aai-and-krastorio-recipe.lua
@@ -13,6 +13,11 @@ util.change_recepie("copper-plate",
                     12)
 
 -- intermediates
+util.change_recepie("motor",
+                    {{"iron-plate", 1}, {"bolts", 3}, {"iron-gear-wheel", 2}},
+                    {{"iron-plate", 2}, {"bolts", 6}, {"transport-belt", 4}},
+                    2,
+                    3)
 util.change_recepie("underground-belt",
                     {{"iron-plate", 2}, {"bolts", 4}, {"transport-belt", 4}},
                     {{"iron-plate", 4}, {"bolts", 8}, {"transport-belt", 4}},
@@ -53,5 +58,20 @@ util.change_recepie("fuel-processor",
 util.change_recepie("electric-mining-drill",
                     {{"iron-beam", 8}, {"iron-gear-wheel", 24}, {"bolts", 20}, {"motor", 6}},
                     {{"iron-beam", 16}, {"iron-gear-wheel", 36}, {"bolts", 28}, {"motor", 10}},
+                    8,
+                    16)
+util.change_recepie("kr-crusher",
+                    {{"iron-beam", 12}, {"iron-gear-wheel", 30}, {"bolts", 36}, {"motor", 10}, {"stone-brick", 16}},
+                    {{"iron-beam", 24}, {"iron-gear-wheel", 60}, {"bolts", 72}, {"motor", 20}, {"stone-brick", 32}},
+                    12,
+                    24)
+util.change_recepie("burner-assembling-machine",
+                    {{"stone-brick", 20}, {"iron-plate", 20}, {"bolts", 24}, {"motor", 4}},
+                    {{"stone-brick", 40}, {"iron-plate", 40}, {"bolts", 40}, {"motor", 8}},
+                    8,
+                    16)
+util.change_recepie("burner-lab",
+                    {{"stone-brick", 12}, {"iron-plate", 20}, {"copper-plate", 16}},
+                    {{"stone-brick", 24}, {"iron-plate", 40}, {"copper-plate", 32}},
                     8,
                     16)
