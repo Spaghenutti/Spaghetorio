b3111e4ca037364aa104a6cbdbfada68bfc1f414
Spaghenutti <147646299+Spaghenutti@users.noreply.github.com>
2024-02-12 21:20:04 +0100

    added settings for wider inventory

--- data.lua
+++ data.lua
@@ -10,3 +10,10 @@ require("prototypes.particle")
 require("prototypes.recipe")
 require("prototypes.resource")
 require("prototypes.technology")  -- add technology back when all items are created :)
+
+data.raw["utility-constants"]["default"].inventory_width = settings.startup["inventory-width"].value
+data.raw["gui-style"]["default"].inventory_scroll_pane.minimal_width = data.raw["utility-constants"]["default"].inventory_width * 40 + settings.startup["inventory-width"].value
+
+data.raw["utility-constants"]["default"].select_slot_row_count = settings.startup["slots-width"].value
+
+data.raw["utility-constants"]["default"].select_group_row_count = settings.startup["group-width"].value
---
+++ settings.lua
@@ -0,0 +1,23 @@
+data:extend({
+  {
+      type = "int-setting",
+      name = "inventory-width",
+      setting_type = "startup",
+      default_value = 10,
+      order = "aa"
+  },
+  {
+      type = "int-setting",
+      name = "slots-width",
+      setting_type = "startup",
+      default_value = 16,
+      order = "ab"
+  },
+  {
+      type = "int-setting",
+      name = "group-width",
+      setting_type = "startup",
+      default_value = 10,
+      order = "ac"
+  }
+})
