ce5335148485dc95b533c78af2e668f5e60d075a
Adrian Steiner <steineadi@gmail.com>
2024-09-28 21:42:17 +0200

    updated run_pipeline (currently bug with calling run_python_pipeline.py)

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -7,13 +7,15 @@ setlocal enabledelayedexpansion
 python -m pytest
 
 :: Capture the exit status
-if %ERRORLEVEL% EQU 0 (echo "Pytest succeeded") else (echo "Pytest failed")
 if not %ERRORLEVEL% EQU 0 (
+    echo "Pytest failed"
     pause
     exit
 )
 
+echo "Pytest succeeded"
+
 :: Run python pipeline
-python -m .\python\run_python_pipeline.py
+python -m %cd%\python\run_python_pipeline.py
 
 pause
