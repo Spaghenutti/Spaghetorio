576715ea7ac7df4586ba488046a54c2f67f14fbc
Adrian Steiner <steineadi@gmail.com>
2024-10-09 12:11:04 +0200

    Added radioactivity

--- control.lua
+++ control.lua
@@ -1,2 +1,8 @@
 
-require('scripts/explorer')
+local explorer = require("scripts.explorer")
+local krastorio_radioactivity = require("scripts.krastorio-radioactivity")
+
+-- Activates upon starting new game
+script.on_init(function()
+    krastorio_radioactivity.add_items()
+end)
