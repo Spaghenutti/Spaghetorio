6b9a695c0ecb74df94882e04421c1e1f13a23286
Adrian Steiner <steineadi@gmail.com>
2025-06-01 08:39:38 +0200

    Fixed that shotgun and shutgun ammo was unlocked twice in tech tree

--- compatibility/factorio-base/technology.lua
+++ compatibility/factorio-base/technology.lua
@@ -1124,15 +1124,15 @@ data.raw.technology["fusion-reactor-equipment"].unit.ingredients = {
 }
 
 --------------------------------------------------------------------------------
--- MARK: Krastorio tech
---------------------------------------------------------------------------------
-
-
-
---------------------------------------------------------------------------------
 -- MARK: Military
 --------------------------------------------------------------------------------
 
+data.raw.technology["military"].effects = {
+  {
+    type = "unlock-recipe",
+    recipe = "submachine-gun"
+  },
+}
 data.raw.technology["military"].prerequisites = {
   "sp-iron-machining"
 }
